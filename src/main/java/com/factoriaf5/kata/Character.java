467a8f516386acdf5a1347f5dd094d193c266b0c
Andrespz07 <andresperezortega2001@gmail.com>
2023-04-07 20:58:01 +0200

    first commit

---
+++ src/main/java/com/factoriaf5/kata/Character.java
@@ -0,0 +1,13 @@
+package com.factoriaf5.kata;
+
+//Iteration One
+public class Character {
+    private int Healt;
+    private int Level;
+    private boolean Alive;
+    public Character(){
+        Healt = 1000;
+        Level = 1;
+        Alive = true;
+    }
+}
