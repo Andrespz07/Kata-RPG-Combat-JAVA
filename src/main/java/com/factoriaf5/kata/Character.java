b21a7d6decbf825f030235945e4760c6f6ac38ee
Andrespz07 <andresperezortega2001@gmail.com>
2023-04-07 22:09:05 +0200

    Characters Class

--- src/main/java/com/factoriaf5/kata/Character.java
+++ src/main/java/com/factoriaf5/kata/Character.java
@@ -1,13 +1,115 @@
 package com.factoriaf5.kata;
+import java.util.HashSet;
+import java.util.Set;
 
-//Iteration One
 public class Character {
-    private int Healt;
-    private int Level;
-    private boolean Alive;
-    public Character(){
-        Healt = 1000;
-        Level = 1;
-        Alive = true;
+    private int health;
+    private int level;
+    private boolean isAlive;
+    private int attackMaxRange;
+    private Set<Character> allies = new HashSet<>();
+    private Set<Character> enemies = new HashSet<>();
+
+    public Character() {
+        health = 1000;
+        level = 1;
+        isAlive = true;
+        attackMaxRange = 2;
+    }
+
+    public int getHealth() {
+        return health;
+    }
+
+    public int getLevel() {
+        return level;
+    }
+
+    public boolean isAlive() {
+        return isAlive;
+    }
+
+    public int getAttackMaxRange() {
+        return attackMaxRange;
+    }
+
+    public void joinFaction(Character character) {
+        allies.add(character);
+        character.getAllies().add(this);
+    }
+
+    public void leaveFaction(Character character) {
+        allies.remove(character);
+        character.getAllies().remove(this);
+    }
+
+    public Set<Character> getAllies() {
+        return allies;
+    }
+
+    public Set<Character> getEnemies() {
+        return enemies;
+    }
+
+    public void attack(Character target, int damage) {
+        if (this == target) {
+            return;
+        }
+
+        int levelDifference = target.getLevel() - this.getLevel();
+        if (levelDifference >= 5) {
+            damage = (int) (damage * 0.5);
+        } else if (levelDifference <= -5) {
+            damage = (int) (damage * 1.5);
+        }
+
+        if (target.getHealth() - damage <= 0) {
+            target.setHealth(0);
+            target.setAlive(false);
+        } else {
+            target.setHealth(target.getHealth() - damage);
+        }
+    }
+
+    public void heal(Character target, int amount) {
+        if (!target.isAlive()) {
+            return;
+        }
+
+        if (this != target) {
+            return;
+        }
+
+        target.setHealth(Math.min(target.getHealth() + amount, 1000));
+    }
+
+    public void takeDamage(int damage) {
+        if (!isAlive()) {
+            return;
+        }
+
+        if (health - damage <= 0) {
+            health = 0;
+            isAlive = false;
+        } else {
+            health -= damage;
+        }
+    }
+
+    public void setAlive(boolean alive) {
+        isAlive = alive;
+    }
+
+    public void setHealth(int health) {
+        this.health = health;
+    }
+
+    public void setLevel(int level) {
+        this.level = level;
+    }
+
+    public void setAttackMaxRange(int attackMaxRange) {
+        this.attackMaxRange = attackMaxRange;
     }
 }
+
--- src/test/java/com/factoriaf5/kata/CharacterTest.java
+++ src/test/java/com/factoriaf5/kata/CharacterTest.java
@@ -1,14 +1,70 @@
 package com.factoriaf5.kata;
+import static org.junit.jupiter.api.Assertions.*;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-public class CharacterTest {
+class CharacterTest {
     
-    @Test
-    public void ExampleTest() {
-        assertEquals(2, 2);
+    private Character character;
+    
+    @BeforeEach
+    void setUp() throws Exception {
+        character = new Character();
     }
 
+    @Test
+    void testDefaultValues() {
+        assertEquals(1000, character.getHealth());
+        assertEquals(1, character.getLevel());
+        assertTrue(character.isAlive());
+        assertEquals(2, character.getAttackMaxRange());
+        assertTrue(character.getAllies().isEmpty());
+        assertTrue(character.getEnemies().isEmpty());
+    }
+    
+    @Test
+    void testJoinFaction() {
+        Character ally = new Character();
+        character.joinFaction(ally);
+        assertTrue(character.getAllies().contains(ally));
+        assertTrue(ally.getAllies().contains(character));
+    }
+    
+    @Test
+    void testLeaveFaction() {
+        Character ally = new Character();
+        character.joinFaction(ally);
+        character.leaveFaction(ally);
+        assertFalse(character.getAllies().contains(ally));
+        assertFalse(ally.getAllies().contains(character));
+    }
+    
+    @Test
+    void testAttack() {
+        Character target = new Character();
+        int initialHealth = target.getHealth();
+        int damage = 100;
+        character.attack(target, damage);
+        assertEquals(initialHealth - damage, target.getHealth());
+    }
+    
+    @Test
+    void testHeal() {
+        character.takeDamage(500);
+        int initialHealth = character.getHealth();
+        int amount = 200;
+        character.heal(character, amount);
+        assertEquals(initialHealth + amount, character.getHealth());
+    }
+    
+    @Test
+    void testTakeDamage() {
+        int initialHealth = character.getHealth();
+        int damage = 1000;
+        character.takeDamage(damage);
+        assertEquals(initialHealth - damage, character.getHealth());
+        assertFalse(character.isAlive());
+    }
 }
+
